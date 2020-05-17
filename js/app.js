e88700e6d0a2279e23dac15252de8d000648ee24
Cecilia <ekomcece@gmail.com>
2020-05-17 07:51:52 +0100

    la balade continue

--- js/app.js
+++ js/app.js
@@ -2,12 +2,16 @@
  * Episode 1
  * Ecrire un programme qui demande à l’utilisateur son adresse e-mail et renvoie un message en cas d’erreur si l’adresse e-mail n’est pas correctement saisie.
 */
-let mail = prompt("Entrez votre adresse mail : exemple@gmail.com")
 
-if (/^[a-z0-9._-]+@[a-z]+\.[a-z]{2,6}$/.test(mail)) {
-    alert("Adresse e-mail valide")
+//Recuperation de l'adresse e-mail
+let mail = prompt("Entrez votre adresse mail : exemple@gmail.com");
+
+//Condition de validation de l'adresse e-mail
+ if (/^[a-zA-Z0-9._-]+@[a-z]+\.[a-z]{2,6}$/.test(mail)) {
+    alert("Adresse e-mail valide");
 } else {
-    alert("Veillez entrer un adresse e-mail valide!")
+        alert("Veillez entrer une adresse e-mail valide!");
+       prompt("Entrez une adresse e-mail à nouveau");
 }
 
 
@@ -16,41 +20,52 @@ if (/^[a-z0-9._-]+@[a-z]+\.[a-z]{2,6}$/.test(mail)) {
  *  Ecrire un programme qui demande à l'utilisateur de renseigner un mot de passe et vérifie si ce mot de passe obéit aux critères suivants
  */
 
-let keyword = prompt('Choisissez un mot de passe')
-//mettre une condition -au moins 8 caracteres; une majuscule et un chiffre *qjouter un while
-while (keyword.length < 8) {
-    if (keyword.length < 8) {
-        alert('votre mot de passe doit contenir au moins 8 caracteres')
-        prompt('Choisissez un mot de valide')
-    } else{
-        break;
-    }
-    
-}
+ //Recuperation du mot de passe
+let keyword = prompt('Choisissez un mot de passe');
 
-
+//Condition de validation du mot de passe
+if(/^[A-Za-z0-9]\w{7,14}$/.test(keyword)) { 
+    alert('Mot de passe valide');
+}else{
+    alert('Votre mot de passe doit contenir au moins 8 caracteres,une majuscule et une nombre');
+    prompt('Choisissez un mot de valide');
+} 
+            
+         
 /**
  * Episode 3
  * Ecrire un programme qui demande à l'utilisateur son nom et le nombre d’enfants qu’il a et affiche un résumé.
 */
 
-let name = prompt('Quel est votre nom?')
-let child = prompt('Combien d\'enfants avez-vous?')
+//Recuperation des informations
+let name = prompt('Quel est votre nom?');
+let child = prompt('Combien d\'enfants avez-vous?');
 
-if ( parsInt(child) == 0) {
-    alert(name + 'Vous n\'avez pas d\'enfants')   
-} else {
-    
+//Affichage du message
+if ( parseInt(child) == 0) {
+    alert(`${name} Vous n\'avez pas d'enfants`);
+} else if (parseInt(child) == 1) {
+    alert(`${name} Vous avez ${child} enfant`);
+} else if (parseInt(child) > 1) {
+    alert(`${name} vous avez ${child} enfants`);
 }
-alert(name + 'Vous avez' + child + 'enfants')
 
 /**
  * Episode 4
  * Ecrire un programme demande la date de naissance d’un utilisateur et vérifie si la date saisie est au format JJ/MM/AAAA.
 */
 
-let birth = prompt('Quel est votre date de naissance?') 
-//au format jj/mm/aaaa
+//Recuperation de la date de naissance
+let birth = prompt('Entrez votre date de naissance au format jj/mm/aaaa');
+
+//Condition de validation de la date de naissance
+if (/^([0-9]{2})\/([0-9]{2})\/([0-9]{4})$/.test(birth)) {
+    alert('Donnee enregistree');
+} else {
+    alert('Veillez entrer une date au bon format');
+    prompt('Entrez votre date de naissance au format jj/mm/aaaa');
+}
+
 
 /**
  * Episode 5
@@ -61,17 +76,45 @@ ajoute une clé titre à cet objet ayant comme valeur M ou Mme en fonction du se
 Affiche l’objet en console.
 */
 
+ 
+let nom = prompt('Quel est votre nom?');
+let prenom = prompt('Quel est votre prénom?');
+let sex = prompt('Quel est votre sexe? Homme ou Femme');
+
+if (sex == 'Homme') {
+    let M = [nom, prenom, sex];
+    console.log(Object.keys(M));
+} if (sex == 'Femme') {
+    let Mme = [nom, prenom, sex];
+    console.log(Object.keys(Mme));
+} else {
+    
+} 
+
+// affichera ['0', '1', '2']
+
 /**
  * Episode 6
  * Ecrire un programme qui demande à l’utilisateur de saisir le numéro d’un jour et renvoie le jour correspondant. 
 */
+
+let day = prompt('Saisissez le numéro d\'un jour de la semaine')
 
 /**
  *Episode 7
  * Ecrire un programme qui demande à l’utilisateur de saisir toutes ses notes en Mathématiques et affiche sa moyenne.
 */
 
+let note1 = prompt('Veillez saisir votre premiere note obtenue en Mathematiques')
+let note2 = prompt('Veillez saisir votre seconde note')
+let note3 = prompt('Veillez saisir votre troisieme note')
+let note4 = prompt('Veillez saisir votre quatrieme note')
+let moyenne = (parseInt(note1) + parseInt(note2) + parseInt(note3) + parseInt(note4))/4
+alert(`Votre moyenne en Mathematique est ${moyenne}`)
+
 /**
  * Episode 8
  * Ecrire un programme qui demande de saisir les noms et les moyennes des élèves d’une classe et affiche le premier de la classe (son nom ainsi que sa moyenne).
 */
+
+let nom_eleve = prompt('Veillez saisir le nom de l\'élève')
